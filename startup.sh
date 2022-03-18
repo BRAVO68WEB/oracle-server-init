13829816c010bc2cdc8a75c8d9295543873fa229
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2022-03-18 15:07:43 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Installing Debian 10 oven current OS"
+echo "Installing Debian 10 over current OS"
 sudo apt install curl -y
 curl -fLO https://raw.githubusercontent.com/bohanyang/debi/master/debi.sh && chmod a+rx debi.sh
 sudo ./debi.sh --version 10 --user root --password "N8@bJggX%fdsJG"
