696d59f94d50348130f1428d0fd67f76dbbc3819
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2022-06-21 16:15:34 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,6 +5,6 @@ sudo apt install curl -y
 curl -fLO https://raw.githubusercontent.com/bohanyang/debi/master/debi.sh && chmod a+rx debi.sh
 
 read -p "Enter your password: " password
-sudo ./debi.sh --version 10 --user root --password "$password"
+sudo ./debi.sh --version 11 --user root --password "$password"
 echo "Ready to restart"
 sudo shutdown -r now
